7b93386fee4db3b8c175eac0edb615487ae2aea1
abdallah <abddoullah.bouzhar@gmail.com>
2023-02-01 23:56:53 +0100

    ok

---
+++ app/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+    // require_once __DIR__."/libraries/Controller.php";
+    // require_once __DIR__."/libraries/Core.php";
+    require_once __DIR__."/utils/headers.php";
+    require_once __DIR__."/utils/utilities.php";
+    // echo "bootstrap";
+    spl_autoload_register(function($classname){
+        require_once ("libraries/".$classname.".php");
+    });
+?>
